ee3c5e6fd68dd508d73fab51b0927d62bdef90f1
felipesty <felipestylex08@gmail.com>
2022-08-23 15:56:48 -0300

    req 1:corrigido

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const express = require('express');
 const app = require('./app');
 require('dotenv').config();
 
+const productController = require('./controllers/productController');
+
+app.use(express.json());
+
+app.get('/products', productController.getAll);
+
+app.get('/products/:id', productController.getProductById);
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
