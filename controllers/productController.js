a81beddb362718940ce8c57d8f689b00cd27e679
felipesty <felipestylex08@gmail.com>
2022-08-24 16:36:49 -0300

    req 12 corrigido.2

--- controllers/productController.js
+++ controllers/productController.js
@@ -35,9 +35,9 @@ const updateProduct = async (req, res) => {
 const deleteProductById = async (req, res) => {
   const { id } = req.params;
   const product = await productService.getProductById(id);
-  
   if (!product) return res.status(404).json({ message: 'Product not found' });
-
+  
+  await product.deleteProductById(id);
   return res.status(204).json();
 };
 
