b0ed9aa54b12037aafd73e0f3769e03d59ff81fb
felipesty <felipestylex08@gmail.com>
2022-08-24 13:11:17 -0300

    req 8

--- models/saleModel.js
+++ models/saleModel.js
@@ -13,7 +13,7 @@ const getSales = async () => {
 
 const getSaleById = async (id) => {
   const [sale] = await connection.execute(
-    `SELECT sp.sale_id AS saleId, s.date, sp.product_id AS productId, sp.quantity
+    `SELECT s.date, sp.product_id AS productId, sp.quantity
     FROM StoreManager.sales AS s
     INNER JOIN StoreManager.sales_products AS sp
     ON sp.sale_id = s.id
