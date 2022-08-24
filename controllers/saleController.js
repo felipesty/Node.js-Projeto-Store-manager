bffa2da6f6db453045cf78cf2d565b694bdc446c
felipesty <felipestylex08@gmail.com>
2022-08-24 15:45:58 -0300

    req 8 corrigido

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -1,7 +1,7 @@
 const saleService = require('../services/saleService');
 
 const getAll = async (_req, res) => {
-  const sales = await saleService.getSales();
+  const sales = await saleService.getAll();
   return res.status(200).json(sales);
 };
 
